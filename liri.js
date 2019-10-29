a99a55ce56795d51d76bace8dbd0009e993ca6d7
Catherine Gomez <catherinegomezstudio@gmail.com>
2019-10-29 18:48:32 -0400

    all APIs working

--- liri.js
+++ liri.js
@@ -1,67 +1,120 @@
+//GOOD CODE BABY
+
 //Hiding keys n bag
 require("dotenv").config();
+
 //Global variables storing required files
 var axios = require("axios");
 var Spotify = require("node-spotify-api");
+var fs = require("fs");
+
+//Keys
+var omdbKey = "20523c31";
 var spotify = new Spotify({
     id: "4c25037bfdcb41b2bc231c354677eae3",
-    secret: "e50f43b8c963423bb5bc1a19d988818c"
-  });
-var moment = require("moment");
-var fs = require("fs");
+    secret: "f5c98ac5366c4c4e86884c1a90950566"
+});
+
 //I got the keys keys keys
 var key = require("./keys.js");
-var spotifyKey = "20523c31";
+
+//console spacer/divider
+var divider = "\n\n------------------------------------------------------------\n\n";
 
 //Positions n such
 var command = process.argv[2];
 var request = process.argv.slice(3).join(" ");
 
-//Movie API function 
 
-function movieApi(movie){
-
-    var divider = "\n------------------------------------------------------------\n\n";
-
-    axios.get('http://www.omdbapi.com/?apikey=' + spotifyKey + '&t=' + movie)
-  .then(function (response) {
-
-    var jsonData = response.data;
-
-    // showData ends up being the string containing the show data we will print to the console
-    var showData = [
-      "Title: " + jsonData.Title,
-      "Year: " + jsonData.Year,
-      "Rating: " + jsonData.Rated,
-      "Genre: " + jsonData.Genre,
-      "Director: " + jsonData.Director,
-      "Plot: " + jsonData.Plot,
-      "Awards: " + jsonData.Awards,
-      "IMDB-Rating: " + jsonData.imdbrating,
-
-    ].join("\n\n");
-
-    // Append showData and the divider to log.txt, print showData to the console
-    fs.appendFile("log.txt", showData + divider, function(err) {
-      if (err) throw err;
-      console.log(showData);
-    });
-  });
+//Liri 
+function liri() {
+    // Print whether searching for a movie or song, print the term as well
+    // By default, if no search type is provided
+    switch (command) {
+        case "movie":
+            if (request) {
+                console.log("Searching for your youth...");
+                omdbApi(request);
+            }
+            else {
+                request = "Moonrise Kingdom";
+                omdbApi();
+            }
+            break;
+        case "song":
+            if (request) {
+                console.log("You should get a better taste in music...");
+                spotifyApi(request);
+            }
+            else {
+                request = "Finesse";
+                spotifyApi();
+            }
+            break;
+        case "concert":
+            console.log(request);
+            if (request) {
+                console.log("You should get better friends...");
+                getConcert(request);
+            }
+            else {
+                request = "Tyler, The Creator";
+                getConcert(request);
+            }
+            break;
+    }
 };
 
-spotifyApi("make that cake");
+//Movie API function 
+function omdbApi() {
 
+    axios.get('http://www.omdbapi.com/?apikey=' + omdbKey + '&t=' + request)
+        .then(function (response) {
 
-//Spotify API function
+            var jsonData = response.data;
 
-function spotifyApi(song) {
- 
-spotify.search({ type: 'track', query: song }, function(err, data) {
-  if (err) {
-    return console.log('Error occurred: ' + err);
-  }
- 
-console.log(JSON.stringify(data, null, 2)); 
-});
+            // showData ends up being the string containing the show data we will print to the console
+            var showData = [
+                "Title: " + jsonData.Title,
+                "Year: " + jsonData.Year,
+                "Rating: " + jsonData.Rated,
+                "Genre: " + jsonData.Genre,
+                "Director: " + jsonData.Director,
+                "Plot: " + jsonData.Plot,
+                "Awards: " + jsonData.Awards,
+                "IMDB-Rating: " + jsonData.imdbrating,
 
+            ].join("\n\n");
+
+            // Append showData and the divider to log.txt, print showData to the console
+            fs.appendFile("log.txt", showData + divider, function (err) {
+                if (err) throw err;
+                console.log(divider + showData + divider);
+            });
+        });
+};
+
+function spotifyApi() {
+
+    spotify.search({ type: 'track', query: request, limit: 1 })
+        .then(function (response) {
+            //   console.log(response);
+            console.log("\n\n Artist: " + response.tracks.items[0].album.artists[0].name + "\n Song Name: " + response.tracks.items[0].name + "\n Album: " + response.tracks.items[0].album.name + "\n Spotify Link: " + response.tracks.items[0].external_urls.spotify + "\n\n");
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
 }
+
+function getConcert() {
+    console.log(request);
+    axios.get("https://rest.bandsintown.com/artists/" + request + "/events?app_id=codingbootcamp").then(
+        function (response) {
+            console.log("\n\n Venue: " + response.data[0].venue.name + "\n City: " + response.data[0].venue.city + ", " + response.data[0].venue.country + "\n Date: " + response.data[0].datetime + "\n\n");
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+};
+
+liri();
